0d29b1ec6a599d4f1ac01c01e5949b140a5ba66f
PMarinPy <pablo.marin1@utp.edu.co>
2024-06-27 17:24:15 -0500

    asdf

--- tmdb/pelisFinal/src/App.jsx
+++ tmdb/pelisFinal/src/App.jsx
@@ -1,11 +1,13 @@
 //import { useState } from 'react'
 import './App.css'
 import SearchComponent from './components/FilterResults'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <>
       <SearchComponent></SearchComponent>
+      <HomePage></HomePage>
     </>
   )
 }
--- tmdb/pelisFinal/src/pages/HomePage.jsx
+++ tmdb/pelisFinal/src/pages/HomePage.jsx
@@ -1,17 +1,56 @@
 
-import TopRatedMovies from '../components/MovieCard.jsx';
-import ActorPopular from '../components/ActorCard.jsx';
-import TopSeriesTV from '../components/SeriesCard.jsx';
+// import TopRatedMovies from '../components/MovieCard.jsx';
+// import ActorPopular from '../components/ActorCard.jsx';
+// import TopSeriesTV from '../components/SeriesCard.jsx';
+import '../style-sheet/HomePage.css';
+import SearchComponent from '../components/FilterResults';
 
-function Home() {
+function HomePage() {
     return (
-      <div className="App">
-        <h1>Top Rated Movies</h1>
-        <TopRatedMovies />
-        <ActorPopular />
-        <TopSeriesTV />
-      </div>
-    );
-  }
+        <div className="container">
+        <header>
+           <div className="logo"></div>
+           
+            <input type="text" placeholder="buscar.." />
+            
+            <p>iniciar sesion</p>
+            <p>ES</p>
+        </header>
   
-  export default Home;
+
+
+        <h1 className="top-pelis">TOP PELICULAS</h1>  
+        <div className="peliculas">
+          <div className="imagen1">
+            <div className="detalles1"></div>
+          </div>
+          <div className="imagen2"><div className="detalles2"></div>
+          </div>
+          <div className="imagen3"></div>
+          <div className="imagen4"></div>   
+             </div>  
+
+
+             <h1 className="top-series">TOP SERIES</h1>  
+             <div className="series">
+            <div className="imagen1"></div>
+            <div className="imagen2"></div>
+            <div className="imagen3"></div>
+            <div className="imagen4"></div>   
+            </div>
+
+
+            <h1 className="top-actores">TOP ACTORES</h1>  
+             <div className="actores">
+            <div className="imagen5"></div>
+            <div className="imagen6"></div>
+            <div className="imagen7"></div>
+            <div className="imagen8"></div>   
+            </div>
+
+
+    </div>
+    );
+}
+  
+export default HomePage;
